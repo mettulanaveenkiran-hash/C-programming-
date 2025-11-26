d1d42b38b01477e43578bf619c3f2230226be0cd
Naveenkiran@123 <mettulanaveenkiran@gmail.com>
2025-11-26 21:27:22 +0530

    Add files via upload

---
+++ sum of all the digits.c
@@ -0,0 +1,24 @@
+Write a Program to Find the sum of all the digits in the Given Number.
+Input Format
+A single line contains an integer N.
+
+Output Format
+Display the given integer digits sum
+
+Constraints
+1=N=1000 
+
+Input-1
+123
+
+
+Output-1
+6
+
+
+Input-2
+1234
+
+
+Output-2
+10
