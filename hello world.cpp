34ffd0c34220a1981979d4c941d2a1fafe5fc226
Naveenkiran@123 <mettulanaveenkiran@gmail.com>
2025-11-26 21:18:13 +0530

    Add files via upload

--- hello world.cpp
+++ hello world.cpp
@@ -1,8 +1,8 @@
-#include<stdio.h>
+#include <stdio.h>
+
 int main()
 {
-   printf("hi iam naveen\n");
-   printf("iam from diwancheruvu\n");
-   printf("my lovely mother name is kumari");
+    printf("Hello World");
+
     return 0;
- } 
+}
---
+++ matrix transpose.cpp
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+	int a[10][10],r,c,i,j;
+	printf("enter row and column size");
+	scanf("%d%d",&r,&c);
+	printf("enter A matrix valyes");
+	for(i=0;i<r;i++)
+	{
+		for(j=0;j<r;j++)
+		{
+			printf("%d",a[i][j]);
+		}
+	}
+	printf("\n");
+	return 0;
+}
+	
+	
